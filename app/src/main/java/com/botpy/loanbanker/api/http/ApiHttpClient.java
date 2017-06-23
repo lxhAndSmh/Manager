3acc13f286b0d6a8d1f45d206fa210f26743ff37
lxhAndSmh <Lxh_smh5558@163.com>
2017-06-23 18:13:14 +0800

    something

--- app/src/main/java/com/botpy/loanbanker/api/http/ApiHttpClient.java
+++ app/src/main/java/com/botpy/loanbanker/api/http/ApiHttpClient.java
@@ -1,8 +1,56 @@
 package com.botpy.loanbanker.api.http;
 
+import com.botpy.loanbanker.BuildConfig;
+import com.botpy.loanbanker.api.LoanApi;
+import com.botpy.loanbanker.api.interceptor.HeaderInterceptor;
+import com.google.gson.GsonBuilder;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
+
 /**
  * Created by liuxuhui on 2017/6/21.
  */
 
 public class ApiHttpClient {
+
+    private final static int TIME_OUT = 5;
+
+    private static ApiHttpClient apiHttpClient;
+
+    private LoanApi loanApi;
+
+    public static ApiHttpClient getInstance(){
+        if(apiHttpClient == null){
+            apiHttpClient = new ApiHttpClient();
+        }
+        return apiHttpClient;
+    }
+
+    public LoanApi getLoanApi(){
+        return loanApi == null ? configRetrofit(LoanApi.class) : loanApi;
+    }
+
+    private <T> T configRetrofit(Class<T> service){
+        Retrofit.Builder builder = new Retrofit.Builder();
+        builder.baseUrl(BuildConfig.API_BASE_URL)
+                .client(confitOkHttpClient())
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create());
+        Retrofit retrofit = builder.build();
+        return retrofit.create(service);
+    }
+
+    private OkHttpClient confitOkHttpClient(){
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        builder.addInterceptor(new HeaderInterceptor())
+                .connectTimeout(TIME_OUT, TimeUnit.SECONDS);
+        return builder.build();
+    }
 }
--- app/src/main/java/com/botpy/loanbanker/api/interceptor/HeaderInterceptor.java
+++ app/src/main/java/com/botpy/loanbanker/api/interceptor/HeaderInterceptor.java
@@ -3,6 +3,7 @@ package com.botpy.loanbanker.api.interceptor;
 import java.io.IOException;
 
 import okhttp3.Interceptor;
+import okhttp3.Request;
 import okhttp3.Response;
 
 /**
@@ -10,8 +11,19 @@ import okhttp3.Response;
  */
 
 public class HeaderInterceptor implements Interceptor {
+
+    private static final String HEADER_APP_ID     = "X-Loan-AppID";
+    private static final String HEADER_DEVICE_ID  = "X-Loan-DeviceID";
+    private static final String HEADER_TIME_STAMP = "X-Loan-Timestamp";
+    private static final String HEADER_SIGNATURE  = "X-Loan-Signature";
+    private static final String HEADER_AUTH       = "Authorization";
+    private static final String HEADER_USER_AGENT = "User-Agent";
+
     @Override
     public Response intercept(Chain chain) throws IOException {
+
+
+        Request.Builder builder = chain.request().newBuilder();
         return null;
     }
 }
